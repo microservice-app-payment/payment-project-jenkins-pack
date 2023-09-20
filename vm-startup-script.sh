8d5f506fa54ecd11476071bf55591db18cc9e637
ngochung1809 <tongochung1809@gmail.com>
2023-09-20 17:17:47 +0700

    update

--- vm-startup-script.sh
+++ vm-startup-script.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
 
-sudo mkdir -p /etc/apt/keyrings
+echo "#### Add Docker's official GPG key ####"
+
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -y -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo '## Add the repository to Apt sources ##'
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
 
-sudo apt-get update
+echo '###  Install Docker in Ubuntu server ##'
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-sudo systemctl enable docker.service
-
-sudo systemctl enable containerd.service
-
-sudo groupadd docker
-
-sudo usermod -aG docker vagrant
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker ubuntu
+sudo chown -R :docker /var/run/docker.sock
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker
