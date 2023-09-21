aad55160b25e7d215edc8f7f8b08da16bad6690e
ngochung1809 <tongochung1809@gmail.com>
2023-09-21 16:33:09 +0700

    update

--- vm-startup-script.sh
+++ vm-startup-script.sh
@@ -23,5 +23,3 @@ sudo usermod -aG docker ubuntu
 sudo chown -R :docker /var/run/docker.sock
 sudo chmod 666 /var/run/docker.sock
 sudo systemctl restart docker
-
-docker exec -it jenkins cat /var/jenkins_home/secrets/initialAdminPassword
